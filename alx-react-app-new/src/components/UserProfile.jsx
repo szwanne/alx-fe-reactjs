83ee526c2d99abd7ab5335fd0fca031aee714ee0
szwanne <103660814+szwanne@users.noreply.github.com>
2025-07-19 19:41:16 +0300

    Fixed the userProfile component

--- alx-react-app-new/src/components/UserProfile.jsx
+++ alx-react-app-new/src/components/UserProfile.jsx
@@ -5,7 +5,9 @@ const UserProfile = (props) => {
       style={{ border: "1px solid gray", padding: "10px", margin: "10px" }}
     >
       <h2 style={{ color: "blue" }}>{props.name}</h2>
-      <p style={{ fontWeight: "bold" }}>Age: {props.age}</p>
+      <p>
+        Age: <span style={{ fontWeight: "bold" }}>{props.age}</span>
+      </p>
       <p>Bio: {props.bio}</p>
     </div>
   );
