b2aae661319d83a1477a23bb3f3bcaaf44cf009d
szwanne <103660814+szwanne@users.noreply.github.com>
2025-07-19 19:38:48 +0300

    Styled the components footer, mainContent and the userProfile

--- alx-react-app-new/src/components/MainContent.jsx
+++ alx-react-app-new/src/components/MainContent.jsx
@@ -1,7 +1,15 @@
 function MainContent() {
   return (
     <main>
-      <p>I love to visit New York, Paris, and Tokyo.</p>
+      <p
+        style={{
+          color: "red",
+          textDecoration: "underline",
+          fontWeight: "bold",
+        }}
+      >
+        I love to visit New York, Paris, and Tokyo.
+      </p>
     </main>
   );
 }
--- alx-react-app-new/src/components/UserProfile.jsx
+++ alx-react-app-new/src/components/UserProfile.jsx
@@ -1,8 +1,11 @@
 const UserProfile = (props) => {
   return (
-    <div className="user-profile-card">
-      <h2>{props.name}</h2>
-      <p>Age: {props.age}</p>
+    <div
+      className="user-profile-card"
+      style={{ border: "1px solid gray", padding: "10px", margin: "10px" }}
+    >
+      <h2 style={{ color: "blue" }}>{props.name}</h2>
+      <p style={{ fontWeight: "bold" }}>Age: {props.age}</p>
       <p>Bio: {props.bio}</p>
     </div>
   );
