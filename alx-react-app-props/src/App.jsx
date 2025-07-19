0185413d2036500ff88225de80b0407afffabbb6
szwanne <103660814+szwanne@users.noreply.github.com>
2025-07-19 22:26:49 +0300

    Updated the app.jsx component

--- alx-react-app-props/src/App.jsx
+++ alx-react-app-props/src/App.jsx
@@ -1,50 +1,19 @@
-import { useState } from "react";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
 import "./App.css";
-import WelcomeMessage from "./components/WelcomeMessage.jsx";
-import Header from "./components/Header.jsx";
-import MainContent from "./components/MainContent.jsx";
-import Footer from "./components/Footer.jsx";
-import UserProfile from "./components/UserProfile.jsx";
-import ProfilePage from "./ProfilePage";
-import { UserContext } from "./UserContext";
+import WelcomeMessage from "./components/WelcomeMessage";
+import Header from "./components/Header";
+import MainContent from "./components/MainContent";
+import Footer from "./components/Footer";
+import ProfilePage from "./components/ProfilePage";
+import { userContext } from "./UserContext";
 
 function App() {
-  const [count, setCount] = useState(0);
-  const userData = { name: "Jane Doe", email: "jane.doe@example.com" };
+  const userData = { name: "Jane Doe", email: "janedoe123@example.com" };
 
   return (
     <>
-      <UserContext.Provider value={userData}>
+      <userContext.Provider value={userData}>
         <ProfilePage />
-      </UserContext.Provider>
-      <ProfilePage userData={userData} />
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-      <WelcomeMessage />
-      <Header />
-      <MainContent />
-      <Footer />
-      <UserProfile name="Alice" age="25" bio="Loves hiking and photography" />
+      </userContext.Provider>
     </>
   );
 }
