7cc95a1ad7e31522f9f2e7814e8f4af6c00e3d49
szwanne <103660814+szwanne@users.noreply.github.com>
2025-08-07 19:34:19 +0300

    Added advanced animations to the userprofile

--- tailwind-react-integration/src/components/UserProfile.jsx
+++ tailwind-react-integration/src/components/UserProfile.jsx
@@ -1,12 +1,14 @@
 function UserProfile() {
   return (
-    <div className="user-profile bg-gray-100 md:p-8 sm:p-4 md:max-w-sm sm:max-w-xs mx-auto ny-20 rounded-lg shadow-lg">
+    <div className="user-profile bg-gray-100 md:p-8 sm:p-4 md:max-w-sm sm:max-w-xs mx-auto ny-20 rounded-lg shadow-lg hover:shadow-xl">
       <img
-        class="rounded-full md:w-36  sm:w-24 sm:h-24 md:h-36 mx-auto"
+        class="rounded-full md:w-36  sm:w-24 sm:h-24 md:h-36 mx-auto hover:scale-110 transition-transform duration-300 ease-in-out"
         src="https://images.pexels.com/photos/771742/pexels-photo-771742.jpeg"
         alt="User"
       />
-      <h1 className="md:text-xl sm:text-lg text-blue-800 my-4">John Doe</h1>
+      <h1 className="md:text-xl sm:text-lg text-blue-800 my-4 hover:text-blue-500">
+        John Doe
+      </h1>
       <p className="text-gray-600 text-base">
         Developer at Example Co. Loves to write code and explore new
         technologies.
