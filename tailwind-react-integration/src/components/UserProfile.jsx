2eda87d9533f396f9b3d78fc6ecf6c790841e820
szwanne <103660814+szwanne@users.noreply.github.com>
2025-08-07 19:24:34 +0300

    Added responsiveness to profile component

--- tailwind-react-integration/src/components/UserProfile.jsx
+++ tailwind-react-integration/src/components/UserProfile.jsx
@@ -1,12 +1,12 @@
 function UserProfile() {
   return (
-    <div className="user-profile bg-gray-100 p-8 max-w-sm mx-auto ny-20 rounded-lg shadow-lg">
+    <div className="user-profile bg-gray-100 md:p-8 sm:p-4 md:max-w-sm sm:max-w-xs mx-auto ny-20 rounded-lg shadow-lg">
       <img
-        class="rounded-full w-36 h-36 mx-auto"
-        src="https://via.placeholder.com/150"
+        class="rounded-full md:w-36  sm:w-24 sm:h-24 md:h-36 mx-auto"
+        src="https://images.pexels.com/photos/771742/pexels-photo-771742.jpeg"
         alt="User"
       />
-      <h1 className="text-xl text-blue-800 my-4">John Doe</h1>
+      <h1 className="md:text-xl sm:text-lg text-blue-800 my-4">John Doe</h1>
       <p className="text-gray-600 text-base">
         Developer at Example Co. Loves to write code and explore new
         technologies.
